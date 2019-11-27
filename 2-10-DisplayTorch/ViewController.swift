aab6a4e5ae5ffbcfde028d56b785dcaf9438254a
Felix Prünte <f.pruente@xu-university.de>
2019-11-27 14:17:25 +0100

    Initial Commit

---
+++ 2-10-DisplayTorch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2-10-DisplayTorch
+//
+//  Created by FelixP on 27.11.19.
+//  Copyright © 2019 FelixP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
